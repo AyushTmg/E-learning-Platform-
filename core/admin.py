154f848c689238c2a40745b7f270cc157fd2e7b7
AyushTmg <ayushtmg20040722@gmail.com>
2023-12-19 17:41:00 +0545

    Feat: Admin interface done

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.db.models import Count
+from django.utils.html import format_html,urlencode
+from django.urls import reverse 
+from .models import Content,Course,Enrollment
 
-# Register your models here.
+class ContentInline(admin.TabularInline):
+    model=Content
+    autocomplete_fields=['course']
+    extra=20
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display=['id','title','duration','price','is_free','created_at','enrollment_count']
+    search_fields = ['title', 'description']
+    list_editable=['price']
+    inlines=[ContentInline]
+    autocomplete_fields=['user']
+    list_per_page=10
+
+    @admin.display(ordering='enrollment_count')
+    def enrollment_count(self,course):
+        url=reverse('admin:core_enrollment_changelist')+"?"+urlencode({"course__id":str(course.id)})
+        return format_html(f'<a href="{url}" target="_blank">{course.enrollment_count}</a>')
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(enrollment_count=Count('enrollment'))
+
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display=['id','course','user','created_at']
+    list_per_page=10
+    autocomplete_fields=['course','user']
+    list_select_related=['user','course']
+
+
+
