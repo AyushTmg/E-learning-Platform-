9d5f38421d4b349abcab97bb90db4548fd644d4d
AyushTmg <ayushtmg20040722@gmail.com>
2024-02-21 17:47:51 +0545

    Feat: Added Models For A Forum

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+
+
+# ! Models For Raising Question In The Forum
+class Question(models.Model):
+    title=models.CharField(max_length=50)
+    description=models.CharField(max_length=200)
+    time_stamp=models.DateTimeField(auto_now_add=True)
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='question')
+
+
+    def __str__(self) -> str:
+        """
+        Retruns String Format For Question Module
+        """
+        return self.title
+    
+
+
+
+# ! Models For Answering Forums Questions 
+class Answer(models.Model):
+    description=models.CharField(max_length=100)
+    time_stamp=models.DateTimeField(auto_now_add=True)
+    question=models.ForeignKey(Question,on_delete=models.CASCADE,realted_name='answer')
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='answer')
+
+
+
+
+# !Like  Model Answer
+class AnswerLike(models.Model):
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name='answer_likes')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('answer', 'user')
+
+
+
+
+# !Like  Model Question
+class QuestionLike(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='lquestion_ikes')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('question', 'user')
+
+
+
