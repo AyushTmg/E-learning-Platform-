eac6cea7996922287dbea0819a77ee7eb501692b
AyushTmg <ayushtmg20040722@gmail.com>
2024-02-22 17:11:36 +0545

    Feat: Added endpoint for  question answer and also added the custom permission to both the question and answer viewset

--- forum/serailizers.py
+++ forum/serailizers.py
@@ -6,6 +6,7 @@ from rest_framework import serializers
 # ! Serializer For Question 
 class QuestionSerailizer(serializers.ModelSerializer):
     user=serializers.StringRelatedField()
+
     class Meta:
         model=Question
         fields=[
@@ -33,12 +34,29 @@ class QuestionSerailizer(serializers.ModelSerializer):
 
 # ! Serailizer For Question Answer
 class AnswerSerailizer(serializers.ModelSerializer):
+    user=serializers.StringRelatedField()
+
     class Meta:
         model=Answer
         fields=[
             'id',
             'user',
-            'question',
             'time_stamp',
             'description',
         ]
+
+
+    def create(self, validated_data):
+        """
+        Overriding the create method to create 
+        a answer instance
+        """
+        question_id=self.context['question_id']
+        user_id=self.context['user_id']
+
+        return Answer.objects.create(
+            user_id=user_id,
+            question_id=question_id,
+            **validated_data
+        )
+
--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,8 @@
-from .models import Question
+from .models import Question,Answer
+from .permissions import IsUserObjectOrAdminPermission
 from .serailizers import (
     QuestionSerailizer,
+    AnswerSerailizer
 )
 
 
@@ -10,9 +12,26 @@ from rest_framework.viewsets import ModelViewSet
 
 #! ViewSet For Question  
 class QuestionViewSet(ModelViewSet):
-    queryset=Question.objects.all().select_related('user')
-    serializer_class=QuestionSerailizer
+    queryset=(
+        Question.objects
+        .all()
+        .select_related('user')
+    )
+    serializer_class=QuestionSerailizer 
+
+    # ! For Specfying Method For ViewSet
+    http_method_names=[
+        'get',
+        'options',
+        'head',
+        'post',
+        'delete'
+    ]
+
+    # ! Custom Permission Called
+    permission_classes=[IsUserObjectOrAdminPermission]
  
+
     def get_serializer_context(self):
         """
         For Passing User_id as serializer Context
@@ -22,6 +41,50 @@ class QuestionViewSet(ModelViewSet):
     
 
 
+# ! ViewSet For Questions Answer
+class AnswerViewSet(ModelViewSet):
+    serializer_class=AnswerSerailizer
+
+    # ! For Specfying Method For ViewSet
+    http_method_names=[
+        'get',
+        'options',
+        'head',
+        'post',
+        'delete'
+    ]
+
+    # ! Custom Permission Called
+    permission_classes=[IsUserObjectOrAdminPermission]
+
+
+    def get_queryset(self):
+        """
+        Overriding the queryset to filter the answer 
+        by the question id present in the URL Parameter 
+        """
+        question_id=self.kwargs['question_pk']
+        return (
+            Answer.objects
+            .filter(question_id=question_id)
+            .select_related('user')
+        )
+
+
+    def get_serializer_context(self):
+        """
+        For Passing User_id as well as the questions 
+        id as the context
+        """
+        user_id=self.request.user.id
+        question_id=self.kwargs['question_pk']
+        return {
+            'user_id':user_id,
+            'question_id':question_id
+        }
+
     
+
+
 
 
